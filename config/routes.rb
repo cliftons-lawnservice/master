c6253be05e99505a0ad51a022111060a1bc572d9
justinweathersby <justinweathersby@gmail.com>
2015-04-03 11:33:06 -0400

    Add layout for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   #You can have the root of your site routed with "root"
   root to: 'home#index'
 
+  # Static Page Routes
+  match '/about',             to: 'home#about',         :via => [:get], :as => 'about'
+  match '/contact',           to: 'home#contactUs',     :via => [:get], :as => 'contact'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
